a606d337e3f7761f0ddbc522d0704b62f5f97b58
vijay <wuweijia2950@gmail.com>
2020-02-28 15:27:03 +0800

    feat: eggjs nuxt 结合

---
+++ nuxt-eggjs/client/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    about
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    // app/controller/home.js下面的test方法
+    const data = await $axios.$get('/test')
+    console.log(data);
+    return { data }
+  }
+}
+</script>
