08767f6eef963935b8e5292b8ce6bd4975649c78
vijay <wuweijia2950@gmail.com>
2020-04-29 13:55:32 +0800

    webpack: 管理资源

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash'
+import './style.css'
+import spiderman from './spiderman.jpg'
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello')
+
+  // 将图像添加到我们现有的 div。
+  var img = new Image();
+  img.src = spiderman;
+
+  element.appendChild(img);
 
   return element;
 }
