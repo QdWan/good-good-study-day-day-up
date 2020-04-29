74465dd9d03a5475b84c4ae4fe96070b18b4d461
vijay <wuweijia2950@gmail.com>
2020-04-29 16:23:09 +0800

    feat: 多环境构建

--- webpack-demo/webpack.common.js
+++ webpack-demo/webpack.common.js
@@ -3,32 +3,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: "./dist",
-    hot: true
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ title: "Title"}),
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  performance: {
-    hints: "warning"
-  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: "production" }),
+  ],
   module: {
     rules: [
       {
         test: /.css$/,
-        use: ["style-loader","css-loader"]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
